4b6d8930fcb9a90fb0f35dab1607634575d2f87a
namnguyenqwe <48663632+namnguyenqwe@users.noreply.github.com>
2020-06-23 11:00:22 +0700

    CmMt

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -8,11 +8,16 @@ namespace BookStoreProject.Models
     public class UserModel
     {
         public string UserName { get; set; }
+
         public string Password { get; set; }
+
         public string Email { get; set; }
         public string FullName { get; set; }
 
-        public bool Status { get; set; }
+        public string AvatarLink { get; set; }
+
+        public bool? Status { get; set; }
+
         public string Role { get; set; }
     }
 }
